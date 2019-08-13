9812044f3cb88fcc2ddccf73841b29c590d72c42
Samer Elkhalid <samer.elkhalid@okstate.edu>
2019-08-13 00:19:16 -0500

    Added Collider to player, added Frictionless material to collider. Camera follows player. Added Jump and move input to player.

---
+++ Assets/Scripts/CameraBounds.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBounds : MonoBehaviour
+{
+    public float minCameraPosX;
+
+    private void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPosX, Mathf.Infinity), transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    public float maxJumpTime = 1f;
+    public Transform groundCheck;
+
+    private float jumpTime = 0f;
+    private bool isGrounded = false;
+    private float curSpeed = 0f;
+    private bool pressedJumpButton = false;
+    private bool jump = false;
+    private Rigidbody2D rigBody;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        rigBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            curSpeed = speed;
+            spriteRenderer.flipX = false;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            curSpeed = -speed;
+            spriteRenderer.flipX = true;
+        }
+        else
+            curSpeed = 0f;
+
+        pressedJumpButton = Input.GetKey(KeyCode.Space);
+    }
+
+    private void FixedUpdate()
+    {
+        rigBody.velocity = new Vector2(curSpeed, rigBody.velocity.y);
+
+        if (!jump)
+        {
+            jumpTime = 0f;
+            isGrounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+            jump = isGrounded && pressedJumpButton;
+        }
+        if (jump)
+        {
+            jumpTime += Time.fixedDeltaTime;
+            Debug.Log(jumpTime);
+            if (jump = jumpTime < maxJumpTime && pressedJumpButton)
+            {
+                rigBody.velocity = new Vector2(rigBody.velocity.x, jumpForce);
+            }
+        }
+    }
+}
