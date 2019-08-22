88b2690750cd1d1d25d6d049cee8439ebcbe1b5f
Samer Elkhalid <samer.elkhalid@okstate.edu>
2019-08-21 22:07:46 -0500

    Added SnakeBot
    Added Idle and Slither animations and Added Script to patrol any platform it's on

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+    public float groundDetectionDistance;
+    public Transform groundCheck;
+
+    private bool movingRight = true;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundHit = Physics2D.Raycast(groundCheck.position, Vector2.down, groundDetectionDistance, LayerMask.GetMask("Ground"));
+
+        if (!groundHit.collider)
+            FlipDirection();
+    }
+
+    private void FlipDirection()
+    {
+        transform.eulerAngles += new Vector3(0f, 180f, 0f);
+        movingRight = !movingRight;
+    }
+}
