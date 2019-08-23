778aa3a7b755fcb7828bb851e2157cb0deca1eef
Samer Elkhalid <samer.elkhalid@okstate.edu>
2019-08-22 21:30:50 -0500

    SnakeBot takes shots
    Blinks white when shot
    Dies after 3 shots
    Explosion animation occurs when killed

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public Material whiteMaterial;
+    public float flashDurationTime;
+    public int maxHealth;
+
+    private int healthRemaining;
+    private SpriteRenderer spriteRenderer;
+    private Material defaultMaterial;
+    private Object explosionRef;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        healthRemaining = maxHealth;
+        defaultMaterial = spriteRenderer.material;
+        explosionRef = Resources.Load("Animations/Explosion");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("PlasmaBullet"))
+        {
+            Destroy(collider.gameObject);
+            healthRemaining--;
+            spriteRenderer.material = whiteMaterial;
+
+            if (healthRemaining <= 0)
+                KillSelf();
+            else
+                Invoke("ResetMaterial", flashDurationTime);
+        }
+    }
+
+    private void ResetMaterial()
+    {
+        spriteRenderer.material = defaultMaterial;
+    }
+
+    private void KillSelf()
+    {
+        GameObject explosion = (GameObject)Instantiate(explosionRef);
+        explosion.transform.position = transform.position;
+        Destroy(gameObject);
+    }
+}
