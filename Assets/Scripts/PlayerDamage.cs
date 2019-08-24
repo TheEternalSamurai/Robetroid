3d867503a6ea72b948c08c3d5bccdfb3675e84e3
Samer Elkhalid <samer.elkhalid@okstate.edu>
2019-08-24 16:20:58 -0500

    Added Player Hit animation with Knockback

--- Assets/Scripts/PlayerDamage.cs
+++ Assets/Scripts/PlayerDamage.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 public class PlayerDamage : MonoBehaviour
 {
     public int maxHealth;
+    public float knockBackTime;
+    public Vector2 damageThrust;
     public Animator anim;
 
+    private Rigidbody2D rigBody;
     private bool hasBeenHit;
     private int healthRemaining;
     private SpriteRenderer spriteRenderer;
@@ -14,6 +17,7 @@ public class PlayerDamage : MonoBehaviour
 
     private void Start()
     {
+        rigBody = GetComponent<Rigidbody2D>();
         hasBeenHit = false;
         spriteRenderer = GetComponent<SpriteRenderer>();
         healthRemaining = maxHealth;
@@ -25,22 +29,37 @@ public class PlayerDamage : MonoBehaviour
     {
         if (collider.CompareTag("Enemy") && !hasBeenHit)
         {
-            Debug.Log("Hit");
-            StartCoroutine("ReceiveDamage", collider.gameObject);
+            StartCoroutine("KnockBack");
+            StartCoroutine("ReceiveDamage");
             if (healthRemaining <= 0)
                 KillSelf();
         }
     }
 
-    private IEnumerator ReceiveDamage(GameObject enemy)
+    private IEnumerator KnockBack()
+    {
+        PlayerController controller = GetComponent<PlayerController>();
+        controller.enabled = false;
+        rigBody.velocity = Vector2.zero;
+        Vector2 knockBackForce = -transform.right * damageThrust.x + transform.up * damageThrust.y;
+        rigBody.AddForce(knockBackForce, ForceMode2D.Impulse);
+        anim.SetBool("hasBeenHit", true);
+
+        yield return new WaitForSeconds(knockBackTime);
+
+        controller.enabled = true;
+        anim.SetBool("hasBeenHit", false);
+    }
+
+    private IEnumerator ReceiveDamage()
     {
         healthRemaining--;
-        Physics2D.IgnoreLayerCollision(gameObject.layer, enemy.layer, true);
+        Physics2D.IgnoreLayerCollision(gameObject.layer, LayerMask.NameToLayer("Enemy"), true);
         hasBeenHit = true;
 
         yield return new WaitForSeconds(3f);
 
-        Physics2D.IgnoreLayerCollision(gameObject.layer, enemy.layer, false);
+        Physics2D.IgnoreLayerCollision(gameObject.layer, LayerMask.NameToLayer("Enemy"), false);
         hasBeenHit = false;
     }
 
